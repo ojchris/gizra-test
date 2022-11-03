5b6ac778a6fe4cd541f879bed4154d8770c01680
Chris Okpada <cokpada37@gmail.com>
2022-11-03 16:19:35 +0100

    Update og and group_node_pevb modules & config

---
+++ web/modules/custom/group_node_pevb/src/EntityViewBuilder/NodeViewBuilderAbstract.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\group_node_pevb\EntityViewBuilder;
+
+use Drupal\pluggable_entity_view_builder\EntityViewBuilderPluginAbstract;
+use Drupal\group_node_pevb\ElementWrapTrait;
+use Drupal\group_node_pevb\ProcessedTextBuilderTrait;
+use Drupal\group_node_pevb\TagBuilderTrait;
+
+/**
+ * An abstract class for Node View Builders classes.
+ */
+abstract class NodeViewBuilderAbstract extends EntityViewBuilderPluginAbstract {
+
+  use ElementWrapTrait;
+  use ProcessedTextBuilderTrait;
+  use TagBuilderTrait;
+
+}
---
+++ web/modules/custom/group_node_pevb/src/Plugin/EntityViewBuilder/NodeGroup.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\group_node_pevb\Plugin\EntityViewBuilder;
+
+use Drupal\node\NodeInterface;
+use Drupal\node\Entity\NodeType;
+use Drupal\group_node_pevb\EntityViewBuilder\NodeViewBuilderAbstract;
+use Drupal\group_node_pevb\LineSeparatorTrait;
+use Drupal\group_node_pevb\TitleAndLabelsTrait;
+
+/**
+ * The "Node Group" plugin.
+ *
+ * @EntityViewBuilder(
+ *   id = "node.group",
+ *   label = @Translation("Node - Group"),
+ *   description = "Node view builder for Group bundle."
+ * )
+ */
+class NodeGroup extends NodeViewBuilderAbstract {
+
+  use LineSeparatorTrait;
+  use TitleAndLabelsTrait;
+
+  /**
+   * Build full view mode.
+   *
+   * @param array $build
+   *   The existing build.
+   * @param \Drupal\node\NodeInterface $entity
+   *   The entity.
+   *
+   * @return array
+   *   Render array.
+   */
+  public function buildFull(array $build, NodeInterface $entity): array {
+
+    // Header.
+    $element = $this->buildHeader($entity);
+    $elements[] = $this->wrapContainerWide($element);
+
+    // Main content and sidebar.
+    $element = $this->buildMainAndSidebar($entity);
+    $elements[] = $this->wrapContainerWide($element);
+
+    $elements = $this->wrapContainerVerticalSpacingBig($elements);
+    $build[] = $this->wrapContainerBottomPadding($elements);
+
+    return $build;
+  }
+
+  /**
+   * Build the header.
+   *
+   * @param \Drupal\node\NodeInterface $entity
+   *   The entity.
+   *
+   * @return array
+   *   Render array
+   *
+   * @throws \IntlException
+   */
+  protected function buildHeader(NodeInterface $entity): array {
+    $elements = [];
+
+    $elements[] = $this->buildConditionalPageTitle($entity);
+
+    // Show the node type as a label.
+    $node_type = NodeType::load($entity->bundle());
+    $elements[] = $this->buildLabelsFromText([$node_type->label()]);
+
+    $elements = $this->wrapContainerVerticalSpacing($elements);
+    return $this->wrapContainerNarrow($elements);
+  }
+
+  /**
+   * Build the Main content and the sidebar.
+   *
+   * @param \Drupal\node\NodeInterface $entity
+   *   The entity.
+   *
+   * @return array
+   *   Render array
+   *
+   * @throws \IntlException
+   */
+  protected function buildMainAndSidebar(NodeInterface $entity): array {
+    $main_elements = [];
+    $sidebar_elements = [];
+
+    // Get the body text, wrap it with `prose` so it's styled.
+    $main_elements[] = $this->buildProcessedText($entity);
+
+    return [
+      '#theme' => 'server_theme_main_and_sidebar',
+      '#main' => $this->wrapContainerVerticalSpacingBig($main_elements),
+      '#sidebar' => $this->wrapContainerVerticalSpacingBig($sidebar_elements),
+    ];
+
+  }
+
+}
