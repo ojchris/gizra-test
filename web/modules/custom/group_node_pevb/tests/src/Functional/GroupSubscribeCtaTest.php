6ea9c8a851c315d2b05bda42fdfbb24603c34247
Chris Okpada <cokpada37@gmail.com>
2022-11-06 11:13:55 +0100

    Check if node is a valid og group, add functional test boilerplate

---
+++ web/modules/custom/group_node_pevb/tests/src/Functional/GroupSubscribeCtaTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\Tests\group_node_pevb\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test description.
+ *
+ * @group group_node_pevb
+ */
+class GroupSubscribeCtaTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stable';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['group_node_pevb'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    // Set up the test here.
+  }
+
+  /**
+   * Test callback.
+   */
+  public function testSomething() {
+    $admin_user = $this->drupalCreateUser(['access administration pages']);
+    $this->drupalLogin($admin_user);
+    $this->drupalGet('admin');
+    $this->assertSession()->elementExists('xpath', '//h1[text() = "Administration"]');
+  }
+
+}
